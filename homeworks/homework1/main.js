900b1cb0e8949fb9edaba8dacf55a0bb5a96e326
DenisOsetrov <dvosetrov@gmail.com>
2024-02-22 10:50:01 +0200

    hw-1, part-1

---
+++ homeworks/homework1/main.js
@@ -0,0 +1,74 @@
+// 1 task
+let hi = 'hello';
+console.log(hi);
+
+let company = 'owu';
+console.log(company);
+
+let email = 'com';
+console.log(email);
+
+let country = 'ua';
+console.log(country);
+
+let number1 = 1;
+console.log(number1);
+
+let number2 = 10;
+console.log(number2);
+
+let number3 = -999;
+console.log(number3);
+
+let number4 = 123;
+console.log(number4);
+
+let number5 = 3.14;
+console.log(number5);
+
+let number6 = 2.7;
+console.log(number6);
+
+let number7 = 16;
+console.log(number7);
+
+let boolean1 = true;
+console.log(boolean1);
+
+let boolean2 = false;
+console.log(boolean2);
+
+
+// 2 task
+let firstName = 'Denis';
+let middleName = 'Osetrov';
+let lastName = 'Volodymyrovych';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+person = firstName + ' ' +middleName + ' ' + lastName;
+console.log(person);
+
+let spase = ' ';
+person = firstName+spase+middleName+spase+lastName+spase;
+console.log(person);
+
+console.log(firstName, middleName, lastName);
+
+person = firstName + middleName + lastName;   // без пробілів
+console.log(person);
+
+
+//3 task
+
+
+
+
+
+
+
+
+
+
+
