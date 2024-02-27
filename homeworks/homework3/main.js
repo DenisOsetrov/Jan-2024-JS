a80e2e79eae4f50e1539b3981847eac0daf3ba13
DenisOsetrov <dvosetrov@gmail.com>
2024-02-27 18:58:20 +0200

    hw-3, part-4

--- homeworks/homework3/main.js
+++ homeworks/homework3/main.js
@@ -182,27 +182,51 @@ document.write(`</div>`)
 
 
 // task 4.0
-// є масив
-// let users = [
-//     {name: 'vasya', age: 31, status: false},
-//     {name: 'petya', age: 30, status: true},
-//     {name: 'kolya', age: 29, status: true},
-//     {name: 'olya', age: 28, status: false},
-//     {name: 'max', age: 30, status: true},
-//     {name: 'anya', age: 31, status: false},
-//     {name: 'oleg', age: 28, status: false},
-//     {name: 'andrey', age: 29, status: true},
-//     {name: 'masha', age: 30, status: true},
-//     {name: 'olya', age: 31, status: false},
-//     {name: 'max', age: 31, status: true}
-// ];
-//  за допомоги циклу вивести:
+// Є масив. За допомоги циклу вивести:
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
 //  - користувачів зі статусом true
+for (const user of users) {
+    if (user.status === true) {
+        console.log(user.name, user.status);
+        document.write(`<div><h3>${user.name} is ${user.status}!</h3></div>`);
+    }
+}
+
 //  - користувачів зі статусом false
-//  - користувачів які старші за 30 років
+for (const user of users) {
+    if (user.status === false){
+        console.log(user.name, user.status);
+        document.write(`<h3>${user.name} is ${user.status}</h3>`)
+    }
+}
 
+//  - користувачів, які старші за 30 років
+for (let j = 0; j < users.length; j++) {
+    const user = users[j];
+    if (user.age > 30) {
+        console.log(user.name, user.age);
+    }
+}
 
-
-
-
-
+//-----------while--------------------------------
+let r = 0;
+while (r < users.length) {
+      if (users[r].age > 30) {
+        console.log(users[r]);
+            document.write(`<div><h4>${users[r].name} - ${users[r].age}</h4></div>`)
+        }
+         r++;
+}
