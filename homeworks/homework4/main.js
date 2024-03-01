e8f6da44e70da602cd3c4dfea0e28ad1505ab6e7
DenisOsetrov <dvosetrov@gmail.com>
2024-03-01 19:15:05 +0200

    hw-4, part-3

--- homeworks/homework4/main.js
+++ homeworks/homework4/main.js
@@ -47,8 +47,9 @@ console.log(rectanglesArea);  // результат роботи ф-ції [50, 
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
 
 function areaOfCircle(r) {
-    return Math.PI*(r**2);
+    return Math.PI * (r ** 2);
 }
+
 // console.log(areaOfCircle(2));
 // використання функції і виведення результату
 let radius1 = 25;
@@ -57,23 +58,23 @@ let radius1Area = areaOfCircle(radius1);
 console.log('Площа кола з радіусом ', radius1, 'дорівнює ', radius1Area);
 
 
-
 // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
 
-function calcAreaCylinder (r,h) {
-    return 2*Math.PI*r*(r+h);
+function calcAreaCylinder(r, h) {
+    return 2 * Math.PI * r * (r + h);
 }
 
 // console.log(calcAreaCylinder(3,5));
 let h = 4;
 let r = 2;
-let qwe1 =calcAreaCylinder(r,h);
+let qwe1 = calcAreaCylinder(r, h);
 console.log('Площа циліндра висотою ', h, 'та радіутом ', r, 'дорівнює ', qwe1);
 
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
-const numbers = [11,22,33,44,55];
-function removes (arr) {
+const numbers = [11, 22, 33, 44, 55];
+
+function removes(arr) {
     for (let i = 0; i < numbers.length; i++) {
         // console.log(i);   // виводить порядковий № масиву
         // console.log(arr)  // виводить кожну ітерацію увесь масив
@@ -81,41 +82,45 @@ function removes (arr) {
         // document.write(`<div><h3>${arr[i]}</h3></div>`);
     }
 }
-removes(numbers);
 
+removes(numbers);
 
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 let text = 'Довільний текст будь-якоро розміру!';
 
-function addText (t) {
+function addText(t) {
     document.write(`
     <div>
     <p>${t}</p>
 </div>`)
 }
+
 addText(text);
 
 // додаю функцію, що має два аргумента (кіл.параграфів, текст)
 //  створюю функцію, що створює параграфи і додає текст
 let paragraphCount = 3;
+
 function addParagraph(p, t) {
     for (let i = 0; i < paragraphCount; i++) {
         document.write(`<p style="color: burlywood">${t}</p>`)
     }
 }
-addParagraph(paragraphCount,text);
+
+addParagraph(paragraphCount, text);
 
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 
-function addUl (t) {
+function addUl(t) {
     document.write(`<ul style="color: coral">
 <li>${t}</li>
 <li>${t}</li>
 <li>${t}</li>
 </ul>`)
 }
+
 addUl('Text');
 
 
@@ -123,7 +128,7 @@ addUl('Text');
 // Текст li задати через аргумент всім однаковий.
 // Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
-function addLi (countLi, t) {
+function addLi(countLi, t) {
     document.write(`<ul style="color: chocolate">`)
 
     for (let i = 0; i < countLi; i++) {
@@ -132,25 +137,98 @@ function addLi (countLi, t) {
 
     document.write(`</ul>`)
 }
-addLi(5,text)
+
+addLi(5, text)
 
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-let arr11 = [12,11,10,'string', true];
+let arr11 = [12, 11, 10, 'string', true];
 
-function printArr (arr) {
+function printArr(arr) {
     document.write(`<ul>`)
     for (const item of arr) {
-       document.write(`<li>${item}</li>`)
+        document.write(`<li>${item}</li>`)
     }
     document.write(`</ul>`)
 }
+
 printArr(arr11)
 
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+let arrUsers = [
+    {id: 1, name: 'Max', age: 31},
+    {id: 2, name: 'Tom', age: 30},
+    // {id: 3, name: 'Nikolas', age: 29},
+    // {id: 4, name: 'Oleg', age: 28},
+    // {id: 5, name: 'Max', age: 30}
+]
+
+function printUsers1(arr) {
+    for (const arrElement of arr) {
+        document.write(`<div>
+            <p>User has id: ${arrElement.id}, name: ${arrElement.name}, age: ${arrElement.age}.</p>
+</div>`)
+    }
+}
+
+printUsers1(arrUsers)
+
+
+// виводить окремо блок і кожен елемент
+function printUsers2(arr) {
+    for (const arrElement of arr) {
+
+        document.write(`<div style="margin: 5px; border: 2px solid rebeccapurple; width: 200px;font-size: 15px; padding-left: 5px">`)
+
+        for (const key in arrElement) {
+            document.write(`<p>${key}: ${arrElement[key]}</p>`)
+        }
+
+        document.write(`</div>`)
+    }
+
+}
+
+printUsers2(arrUsers);
+
+
 // - створити функцію яка повертає найменьше число з масиву
+let summa = [2, 3, 6, 9, 1, 8, 13];
+
+function minMaxValue(arr) {
+    let min = arr[0];
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+return {min, max};
+}
+
+console.log(minMaxValue(summa));
+
+
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+
+let sum = [10,20,70];
+
+function calculateSum (arr) {
+    let sumElement = 0;
+    for (const arrElement of arr) {
+        sumElement += arrElement;
+    }
+    return sumElement;
+}
+
+console.log(calculateSum(sum))
+
+
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
